3897e8eef7069ab0db7f01c12b38d5e0204ab910
medina325 <medina_cdz@hotmail.com>
2025-02-03 11:20:34 +0800

    Melhorando navegação ao configurar scrollBehavior nas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -8,63 +8,71 @@ import EventEditView from "@/views/events/Edit.vue";
 import NotFoundView from "@/views/NotFound.vue";
 import NetworkErrorView from "@/views/NetworkError.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "event-list",
+    props: (router) => ({
+      page: parseInt(router.query.page) || 1,
+      limit: parseInt(router.query.limit) || 4,
+    }),
+    component: ListView,
+  },
+  {
+    path: "/events/:id",
+    name: "event-layout",
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: "",
+        name: "event-details",
+        component: EventDetailsView,
+      },
+      {
+        path: "register",
+        name: "event-register",
+        component: EventRegisterView,
+      },
+      {
+        path: "edit",
+        name: "event-edit",
+        component: EventEditView,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/network-error",
+    name: "network-error",
+    component: NetworkErrorView,
+  },
+  {
+    path: "/404/:resource",
+    name: "not-found-resource",
+    component: NotFoundView,
+    props: true,
+  },
+  {
+    path: "/:catchAll(.*)*",
+    name: "not-found",
+    component: NotFoundView,
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "event-list",
-      props: (router) => ({
-        page: parseInt(router.query.page) || 1,
-        limit: parseInt(router.query.limit) || 4,
-      }),
-      component: ListView,
-    },
-    {
-      path: "/events/:id",
-      name: "event-layout",
-      props: true,
-      component: EventLayout,
-      children: [
-        {
-          path: "",
-          name: "event-details",
-          component: EventDetailsView,
-        },
-        {
-          path: "register",
-          name: "event-register",
-          component: EventRegisterView,
-        },
-        {
-          path: "edit",
-          name: "event-edit",
-          component: EventEditView,
-        },
-      ],
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: AboutView,
-    },
-    {
-      path: "/network-error",
-      name: "network-error",
-      component: NetworkErrorView,
-    },
-    {
-      path: "/404/:resource",
-      name: "not-found-resource",
-      component: NotFoundView,
-      props: true,
-    },
-    {
-      path: "/:catchAll(.*)*",
-      name: "not-found",
-      component: NotFoundView,
-    },
-  ],
+  routes,
+  scrollBehavior(to, _, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { top: 0 };
+  },
 });
 
 router.resolve({
